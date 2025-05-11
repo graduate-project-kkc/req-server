8d2ebcab5f61f5882b2ecbb184dcdf829edc9e56
beomzun <punpon53@naver.com>
2025-05-11 14:41:59 +0900

    feat: implement streaming upload and processing

---
+++ src/main/java/graduate/req_server/domain/image/controller/ImageController.java
@@ -0,0 +1,26 @@
+package graduate.req_server.domain.image.controller;
+
+import graduate.req_server.domain.image.dto.request.ImageRequest;
+import graduate.req_server.domain.image.dto.response.ImageResponse;
+import graduate.req_server.domain.image.service.ImageService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/images")
+@RequiredArgsConstructor
+public class ImageController {
+
+    private final ImageService imageService;
+
+    @PostMapping
+    public ResponseEntity<ImageResponse> uploadImage(@ModelAttribute ImageRequest request) {
+        ImageResponse responses = imageService.uploadAndProcess(request);
+        return ResponseEntity.ok(responses);
+    }
+}
