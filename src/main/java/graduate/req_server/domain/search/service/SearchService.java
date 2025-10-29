d7f1300423821379bc11c06b7aac3afa9a2aaecc
Bzun <punpon53@naver.com>
2025-10-29 22:24:36 +0900

    feat: 검색어 번역 기능 추가

--- src/main/java/graduate/req_server/domain/search/service/SearchService.java
+++ src/main/java/graduate/req_server/domain/search/service/SearchService.java
@@ -6,6 +6,7 @@ import graduate.req_server.domain.search.dto.response.SearchResponse;
 import graduate.req_server.util.client.ai.AiClient;
 import graduate.req_server.util.client.pinecone.PineconeClient;
 import graduate.req_server.util.client.s3.S3Service;
+import graduate.req_server.util.client.translate.TranslationService;
 import io.pinecone.unsigned_indices_model.ScoredVectorWithUnsignedIndices;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -20,13 +21,16 @@ public class SearchService {
     private final AiClient aiClient;
     private final PineconeClient pineconeClient;
     private final S3Service s3Service;
+    private final TranslationService translationService;
 
     public SearchResponse searchByText(SearchRequest request) {
         log.debug("[SearchService] searchByText");
 
         double minScore = 0.1;
 
-        List<Float> vector = aiClient.textToVector(request.getQuery());
+        String query = translationService.translate(request.getQuery(), "auto", "en");
+        log.info("translation result: {}", query);
+        List<Float> vector = aiClient.textToVector(query);
         List<ScoredVectorWithUnsignedIndices> matches = pineconeClient.queryTopKWithScore(vector);
 
         List<PhotoInfo> photos = matches.stream()
