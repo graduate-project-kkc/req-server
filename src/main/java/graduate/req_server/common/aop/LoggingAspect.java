eb625a1fb8d3c56051c7ab122186318fa902fc7c
Bzun <punpon53@naver.com>
2025-10-29 22:18:54 +0900

    feat: AOP 로깅 추가

---
+++ src/main/java/graduate/req_server/common/aop/LoggingAspect.java
@@ -0,0 +1,31 @@
+package graduate.req_server.common.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Pointcut("within(@org.springframework.stereotype.Service *)")
+    public void serviceMethods() {}
+
+    @Around("serviceMethods()")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+
+        Object result = joinPoint.proceed();
+
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+
+        log.info("{} executed in {} ms", joinPoint.getSignature(), executionTime);
+
+        return result;
+    }
+}
