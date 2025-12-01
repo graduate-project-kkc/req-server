3fbc05437ff5d2aff2da863c6b70522335e6514f
friend1226 <friendcha7749@gmail.com>
2025-12-01 16:59:19 +0900

    fix: 파일 전송 시 content-type 명시 제거

--- src/main/resources/static/api.js
+++ src/main/resources/static/api.js
@@ -21,10 +21,31 @@ async function apiGet(endpoint) {
     }
 }
 
-async function apiPost(endpoint, data, _contentType) {
+async function apiPost(endpoint, data) {
     let init = {
         method: "POST",
-        headers: { "Content-Type": _contentType || "application/json" },
+        headers: { "Content-Type": "application/json" },
+        body: data,
+    };
+    if (localStorage.getItem("accessToken")) {
+        init.headers["Authorization"] = "Bearer " + localStorage.getItem("accessToken");
+    }
+
+    const response = await fetch(`${API_BASE_URL}${endpoint}`, init);
+    if (!response.ok) {
+        throw new Error(`POST ${endpoint} 실패`);
+    }
+    const contentType = response.headers.get("Content-type");
+    if (contentType && contentType.includes("application/json")) {
+        return await response.json();
+    } else {
+        return null;
+    }
+}
+
+async function apiPostFile(endpoint, data) {
+    let init = {
+        method: "POST",
         body: data,
     };
     if (localStorage.getItem("accessToken")) {
